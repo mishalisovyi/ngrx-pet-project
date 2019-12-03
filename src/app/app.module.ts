17c3f7d6731829c60951a8d1743add447b946ef0
mishalisovyi <mishalisovyi@gmail.com>
2019-12-04 01:17:42 +0200

    finish page 32

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { RouterModule } from '@angular/router';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StarshipsModule } from 'src/app/starships/starships.module';
 import { reducers, metaReducers } from './store/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -12,6 +14,8 @@ import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './store/effects/auth.effects';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { CustomSerializer } from 'src/app/store/serializers/router.serializer';
+import { ShipListComponent } from './starships/ship-list/ship-list.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +33,13 @@ import { WelcomeComponent } from './welcome/welcome.component';
         strictStateImmutability: true,
         strictActionImmutability: true,
       }
+    }),
+    RouterModule.forRoot([
+      { path: '', component: ShipListComponent },
+      { path: 'aaa', component: ShipListComponent }
+    ]),
+    StoreRouterConnectingModule.forRoot({
+      serializer: CustomSerializer
     }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([AuthEffects])
--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -7,18 +7,25 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 import * as fromAuth from './auth.reducer';
+import * as fromRouter from 'src/app/store/serializers/router.serializer';
+import { RouterReducerState, routerReducer } from '@ngrx/router-store';
 
 
 export interface State {
   [fromAuth.authFeatureKey]: fromAuth.State;
+  [fromRouter.routerFeatureKey]: RouterReducerState<fromRouter.RouterStateUrl>;
 }
 
 export const reducers: ActionReducerMap<State> = {
   [fromAuth.authFeatureKey]: fromAuth.reducer,
+  [fromRouter.routerFeatureKey]: routerReducer
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
 
-export const selectAuthState = createFeatureSelector<fromAuth.State>('auth');
+export const selectAuthState = createFeatureSelector<fromAuth.State>(fromAuth.authFeatureKey);
+export const selectReducerState = createFeatureSelector<RouterReducerState<fromRouter.RouterStateUrl>>(fromRouter.routerFeatureKey);
+
 export const getUserName = createSelector(selectAuthState, fromAuth.getUserName);
 export const getFriendlyName = createSelector(selectAuthState, fromAuth.getFriendlyName);
+export const getRouterInfo = createSelector(selectReducerState, state => state.state);
